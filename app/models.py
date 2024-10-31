6b1ffd453f27eb9fba3a420335f48d50c20bab36
Odilbek Arziev <odarziev@mail.ru>
2024-10-31 12:06:18 +0300

    model post added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    slug = models.SlugField(unique=True)
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
