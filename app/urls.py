2b286602208607d7ba770e588af247ac971acdc5
Odilbek Arziev <odarziev@mail.ru>
2024-11-28 12:07:49 +0300

    base_layout added and some styles

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', home),
-    path('post_detail/<int:pk>', post_detail, name='post_detail')
+    path('', home, name='home'),
+    path('post_detail/<int:pk>', post_detail, name='post_detail'),
+    path('post_create/', post_create, name='post_create'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import HttpResponse
+from .forms import PostForm
 
 
 def home(request):
     posts = Post.objects.all()
     return render(request, "home.html", {"posts": posts})
 
+
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
-    return render(request, 'post.html', {'post': post})
+    return render(request, "post.html", {"post": post})
+
+
+def post_create(request):
+    form = PostForm()
+    return render(request, "post_create.html", {'form': form})
